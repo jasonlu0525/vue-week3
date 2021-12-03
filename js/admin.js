12f0967f51cb5d10afec960cf6a8bbf959223763
jasonlu0525 <51610162+jasonlu0525@users.noreply.github.com>
2021-12-04 01:14:41 +0800

    完成 post 功能，但表單驗證有 bug 待修

--- js/admin.js
+++ js/admin.js
@@ -8,12 +8,17 @@ createApp({
             products: [],
             userRequest: "",
             mainProductUrl: '',
-            subProductUrl: '',
+            subProductUrl: {
+                tempName: '',
+                collection: []
+            },
             addNewData: {},
+            errorMessage: {},
             deletdDataID: '',
             sysTemLoader: {
                 title: "",
-                message: ""
+                message: "",
+                isSuccess: false
             }
         }
     },
@@ -28,13 +33,14 @@ createApp({
 
                 }).catch((err) => {
                     console.dir(err);
-                    if (!err.response.data.success) {
-                        alert(err.response.data.message);
+                    if (!err.data.success) {
+                        alert(err.data.message);
                         location = "index.html"
                     }
 
                 })
                 .then((res) => { // 驗證登入狀態後取得產品資料
+                    console.log(res);
                     console.log(res.data.products);
                     this.products = res.data.products
 
@@ -52,7 +58,9 @@ createApp({
             console.log(url, {
                 data: config
             });
-            this.userRequest[method](url, config).then((res) => {
+            this.userRequest[method](url, {
+                data: config
+            }).then((res) => {
                 return axios.get("https://vue3-course-api.hexschool.io/v2/api/jason/admin/products"); // Promise 鏈接
             }).catch((err) => {
                 console.log(err);
@@ -63,30 +71,147 @@ createApp({
             })
 
         },
-        deletdData() {
-            this.sysTemLoader = {
-                title: "刪除結果",
-                message: "成功 !!"
 
-            }
+        deletdData() {
+
+            this.products.forEach((element, index) => {
+                console.log(element.id === this.deletdDataID);
+                if (element.id === this.deletdDataID) {
+
+                    this.products.splice(index, 1)
+                }
+
+            });
+
+            this.sysTemLoader.title = "刪除結果";
+            this.sysTemLoader.message = "成功 !!";
+
+
+            this.showModal("#loading");
 
             setTimeout(() => {
-                this.showModal("#loading");
+                console.log(this);
+                this.closeModal("#loading");
             }, 1000);
+
 
             this.Axios('delete', `/api/jason/admin/product/${ this.deletdDataID}`)
 
         },
+        //建立新的產品 --> 取消
         addNewDataCanceled() {
             this.addNewData = {}
+            this.errorMessage = {}
         },
+        //建立新的產品 --> 確認
+        addNewDataConfirm() {
+
+            const error = this.formVadidate();
+            console.log(error);
+            this.errorMessage = error;
+
+            if (!error) { // 表單驗證沒有錯誤
+
+                this.addNewData.imagesUrl = this.subProductUrl.collection;
+
+                this.products.unshift(this.addNewData)
+
+                console.log(this.addNewData);
+
+                this.Axios('post', `/api/jason/admin/product`, this.addNewData)
+                this.errorMessage = undefined;
+            }
+
+
+
+        },
+        // 建立新的產品 -->新增主圖
         addMainImg() {
             this.addNewData.imageUrl = this.mainProductUrl;
         },
+        //建立新的產品 --> 移除主圖
         removeMainImg() {
             this.addNewData.imageUrl = '';
             this.mainProductUrl = '';
         },
+        // 建立新的產品 -->新增副圖
+        addSubImg() {
+            if (this.subProductUrl.tempName === "") {
+                return
+            }
+
+            this.subProductUrl.collection.push(this.subProductUrl.tempName)
+            this.subProductUrl.tempName = "";
+
+        },
+        //建立新的產品 -->選擇副圖-->  移除副圖
+        selectSubImage(SubimgUrl) {
+            this.subProductUrl.tempName = SubimgUrl;
+        },
+        //建立新的產品 -->移除副圖
+        removeSubImg() {
+            this.subProductUrl.collection.forEach((item, index, arr) => {
+                if (this.subProductUrl.tempName === item) {
+                    arr.splice(index, 1)
+                    this.subProductUrl.tempName = "";
+
+                }
+            })
+
+        },
+        formVadidate() {
+            const constraints = {
+                title: {
+                    presence: {
+                        message: "為必填"
+                    },
+                    length: {
+                        minimum: 4
+                    }
+                },
+                category: {
+                    presence: {
+                        message: "為必填"
+                    },
+                    length: {
+                        minimum: 4
+                    }
+                },
+                unit: {
+                    presence: {
+                        message: "為必填"
+                    },
+                    length: {
+                        minimum: 1
+                    }
+                },
+                origin_price: {
+                    presence: {
+                        message: "為必填"
+                    },
+                    numericality: {
+                        greaterThanOrEqualTo: 0
+                    }
+                },
+                price: {
+                    presence: {
+                        message: "為必填"
+                    },
+                    numericality: {
+                        greaterThanOrEqualTo: 0
+                    }
+                }
+            }
+
+            return validate({
+                title: this.addNewData.title,
+                category: this.addNewData.category,
+                unit: this.addNewData.unit,
+                origin_price: this.addNewData.origin_price,
+                price: this.addNewData.origin_price,
+
+            }, constraints);
+        }
 
     },
 
@@ -96,6 +221,8 @@ createApp({
             baseURL: 'https://vue3-course-api.hexschool.io/v2',
 
         })
+        console.log(this.sysTemLoader);
+
     },
 
 
